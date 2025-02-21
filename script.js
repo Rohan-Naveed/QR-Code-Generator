75474abafb7b9fa4c17f54f433f83ab31655ba65
Rohan Naveed <rohan.naveed626899@gmail.com>
2025-02-21 23:57:00 +0500

    Finished QR Generator

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const container = document.querySelector(".container");
 const form = document.querySelector("form");
 const input = document.querySelector("input");
 const qrBox = document.querySelector(".qr-box");
+const textBox = document.querySelector(".text-box");
 
 // Create multiple stars at random positions with varied animation durations
 for (let i = 0; i < 100; i++) {
@@ -14,45 +15,98 @@ for (let i = 0; i < 100; i++) {
   document.body.appendChild(star);
 }
 
-// Functions
+let URL;
+
 const storeLink = function () {
-  // const URL = input.value;
-  const URL = "hhjbjkjnnj";
+  URL = input.value;
   input.blur();
   console.log(URL);
 };
 
-const checkURL = function (url) {};
+/////////// SECTION URL VALIDATORS
 
-const createQRCode = async function () {
-  // new QRCode(qrBox, URL);
-  let qr = await new QRCode(qrBox, URL);
+function isValidURL(url) {
+  const pattern = /^(https?|ftp|mailto):\/\/[^\s/$.?#].[^\s]*$/i;
+  return pattern.test(url);
+}
 
-  if (!qr) {
-    console.log("ERROR");
-  }
+function isLengthValid(url) {
+  return url.length <= 2953;
+}
 
-  console.log(qr);
+const checkURL = function (url) {
+  return isValidURL(url) && isLengthValid(url);
 };
 
+//////// SECTION CONTAINER SIZE UPDATE
 const containerSizeSuccess = function () {
   container.style.height = "450px";
 };
 
+const containerSizeFailure = function () {
+  container.style.height = "290px";
+  container.style.width = "300px";
+};
+
+////////// SECTION BOX HIDE/DISPLAY
 const qrBoxDisplay = function () {
   qrBox.style.display = "block";
 };
 
+const qrBoxHide = function () {
+  qrBox.style.display = "none";
+};
+
+const textBoxDisplay = function () {
+  textBox.style.display = "block";
+};
+
+const textBoxHide = function () {
+  textBox.style.display = "none";
+};
+
+/////////// Empty QR Box
+const emptyQRBox = function () {
+  qrBox.innerHTML = "";
+};
+
+/////////// SECTION QR CODE CREATION
+const createQRCode = async function () {
+  // First Empty QR Box
+  emptyQRBox();
+
+  let qr = await new QRCode(qrBox, URL);
+  console.log(qr);
+};
+
+//////////// SECTION MAIN FUNCTION
 form.addEventListener("submit", function () {
   // Store Link
   storeLink();
 
-  // Changing Container Layout
-  containerSizeSuccess();
+  console.log(checkURL(URL));
 
-  // Generate QR Code
-  createQRCode();
+  if (checkURL(URL)) {
+    // Hide Text Box
+    textBoxHide();
 
-  // Displaying QR Code
-  qrBoxDisplay();
+    // Changing Container Layout
+    containerSizeSuccess();
+
+    // Generate QR Code
+    createQRCode();
+
+    // Displaying QR Code
+    qrBoxDisplay();
+  } else {
+    console.log("FAILURE");
+    // QR Box Hide
+    qrBoxHide();
+
+    // Changing Container Layout
+    containerSizeFailure();
+
+    // Text Box Display
+    textBoxDisplay();
+  }
 });
